ac2076440f3a71106a35a42002a5769ab9017a02
tawny <hitawnie@gmail.com>
2023-03-05 22:16:21 -0800

    add tcp  listener to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
+
