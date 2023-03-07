864d16fd9ffb54eae2cd2737bbf4cb6d1f28f93f
tawny <hitawnie@gmail.com>
2023-03-07 00:41:43 -0800

    Handle errors without panic on server start.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(1);
+    });
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
