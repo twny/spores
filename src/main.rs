332a8e9eb08e18e9d08ac51f5ef5a583a1dcab4b
tawny <hitawnie@gmail.com>
2023-03-07 00:44:20 -0800

    Add handle_connection
    Parses the stream line by line and logs to stdout.
    Writes a byte response to the client.
    Return 200 status code.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::net::TcpListener;
+use std::process;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap_or_else(|err| {
@@ -8,8 +12,22 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        handle_connection(stream);
 
         println!("Connection established!");
     }
 }
 
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf = BufReader::new(&mut stream);
+    let req: Vec<_> = buf.lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request {:#?}", req);
+    let resp = "HTTP/1.1 200 OK \r\n\r\n";
+    stream.write_all(resp.as_bytes()).unwrap();
+
+}
