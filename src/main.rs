c5617611f3b800c4ccfee0c810548106124e2c49
tawny <hitawnie@gmail.com>
2023-03-13 00:38:04 -0700

    Add route validation and 404 page

--- src/main.rs
+++ src/main.rs
@@ -27,22 +27,43 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request {:#?}", req);
-    let body = match fs::read_to_string("src/root.html") {
-        Ok(r) => r,
-        Err(err) => {
-            println!("{err}");
-            return;
+    let route = "GET / HTTP/1.1";
+    if req.first().is_some() && req[0] == route {
+        println!("Request {:#?}", req);
+        let body = match fs::read_to_string("src/index.html") {
+            Ok(r) => r,
+            Err(err) => {
+                println!("{err}");
+                return;
+            }
+        };
+        let status = "HTTP/1.1 200 OK \r\n";
+        let size = format!("Content-Length: {}\r\n", body.len());
+        let response = format!("{status}{size}\r\n{body}");
+        match stream.write_all(response.as_bytes()) {
+            Ok(r) => r,
+            Err(err) => {
+                println!("{err}");
+                return;
+            }
         }
-    };
-    let status = "HTTP/1.1 200 OK \r\n";
-    let size = format!("Content-Length: {}\r\n", body.len());
-    let response = format!("{status}{size}\r\n{body}");
-    match stream.write_all(response.as_bytes()) {
-        Ok(r) => r,
-        Err(err) => {
-            println!("{err}");
-            return;
+    } else {
+        let status = "HTTP/1.1 404 Not Found \r\n";
+        let body = match fs::read_to_string("src/404.html") {
+            Ok(r) => r,
+            Err(err) => {
+                println!("{err}");
+                return;
+            }
+        };
+        let size = format!("Content-Length: {}\r\n", body.len());
+        let response = format!("{status}{size}\r\n{body}");
+        match stream.write_all(response.as_bytes()) {
+            Ok(r) => r,
+            Err(err) => {
+                println!("{err}");
+                return;
+            }
         }
     }
 }
